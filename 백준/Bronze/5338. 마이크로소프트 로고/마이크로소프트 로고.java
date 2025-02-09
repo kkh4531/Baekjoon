67b9302f1e74e1583ad2ee7ca1dd1321e767bed0
kkh4531 <rlgus4531@naver.com>
2025-02-09 23:37:54 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 96 ms, Memory: 14088 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("       _.-;;-._").append('\n');
+        stringBuilder.append("'-..-'|   ||   |").append('\n');
+        stringBuilder.append("'-..-'|_.-;;-._|").append('\n');
+        stringBuilder.append("'-..-'|   ||   |").append('\n');
+        stringBuilder.append("'-..-'|_.-''-._|");
+        System.out.println(stringBuilder);
+    }
+}
