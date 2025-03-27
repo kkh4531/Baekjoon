acf2ae095148ce940b20b60c197124c8b82c250a
kkh4531 <rlgus4531@naver.com>
2025-03-27 20:32:52 +0900

    [Silver II] Title: 쇠막대기, Time: 156 ms, Memory: 16620 KB -BaekjoonHub

---
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static int solution(String str) {
+        int cnt = 0;
+        Deque<Character> dq = new ArrayDeque<>();
+        dq.addLast(str.charAt(0));
+        for (int i = 1; i < str.length(); i++) {
+            if (str.charAt(i) == '(') {
+                dq.addLast('(');
+            } else { // )
+                if (str.charAt(i - 1) == '(') { // 레이저
+                    dq.removeLast();
+                    cnt += dq.size();
+                } else { // 쇠막대기의 끝
+                    dq.removeLast();
+                    cnt++;
+                }
+            }
+        }
+        return cnt;
+    }
+    public static void main(String[] args) throws Exception {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = bufferedReader.readLine();
+
+        System.out.println(solution(str));
+    }
+
+}
