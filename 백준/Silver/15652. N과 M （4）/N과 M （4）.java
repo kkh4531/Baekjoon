1e98707db904993b142d53a6fabf8451d6b2d7fc
kkh4531 <rlgus4531@naver.com>
2025-04-17 21:31:02 +0900

    [Silver III] Title: N과 M (4), Time: 128 ms, Memory: 17852 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static int[] arr;
+    public static boolean[] visit;
+    public static StringBuilder sb = new StringBuilder();
+    public static int N;
+    public static int M;
+
+    public static void dfs(int at, int depth) {
+        if (depth == M) {
+            for (int val : arr) {
+                sb.append(val).append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
+        for (int i = at; i < N; i++) {
+            //if (!visit[i]) {
+            //visit[i] = true;
+            arr[depth] = i + 1;
+            dfs(i, depth + 1);
+            //visit[i] = false;
+            //}
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bufferedReader.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[M];
+        visit = new boolean[N];
+        dfs(0, 0);
+        System.out.println(sb);
+    }
+}
