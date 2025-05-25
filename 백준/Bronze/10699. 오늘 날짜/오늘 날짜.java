50defe1ae1518f9dfd7c97e184b53cfc8e4344e0
kkh4531 <rlgus4531@naver.com>
2025-05-25 23:16:08 +0900

    [Bronze V] Title: 오늘 날짜, Time: 168 ms, Memory: 17204 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println(LocalDate.now());
+    }
+}
