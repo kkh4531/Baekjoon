0f3039d2a4bdcf7c6fd41e4e90e80058cbafdb4f
kkh4531 <rlgus4531@naver.com>
2025-06-09 21:49:16 +0900

    [Bronze V] Title: 고려대는 사랑입니다, Time: 100 ms, Memory: 13960 KB -BaekjoonHub

---
+++ 백준/Bronze/11942. 고려대는 사랑입니다/고려대는 사랑입니다.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("고려대학교");
+    }
+}
