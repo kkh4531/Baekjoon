ffa8edcd9279e4e1f679f1aca8dfeb01d62cda6f
kkh4531 <rlgus4531@naver.com>
2025-08-08 23:37:50 +0900

    [Bronze V] Title: 홍익대학교, Time: 172 ms, Memory: 17708 KB -BaekjoonHub

---
+++ 백준/Bronze/16394. 홍익대학교/홍익대학교.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        Scanner scanner = new Scanner(System.in);
+        int i = scanner.nextInt();
+        System.out.println(i - 1946);
+    }
+}
