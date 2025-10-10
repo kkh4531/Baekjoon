b0a490cc1500f0dc07341fc74906e8c7d54d499c
kkh4531 <rlgus4531@naver.com>
2025-10-10 23:44:50 +0900

    [Bronze V] Title: Rook, Time: 100 ms, Memory: 14180 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("  ___  ___  ___").append('\n');
+        sb.append("  | |__| |__| |").append('\n');
+        sb.append("  |           |").append('\n');
+        sb.append("   \\_________/").append('\n');
+        sb.append("    \\_______/").append('\n');
+        sb.append("     |     |").append('\n');
+        sb.append("     |     |").append('\n');
+        sb.append("     |     |").append('\n');
+        sb.append("     |     |").append('\n');
+        sb.append("     |_____|").append('\n');
+        sb.append("  __/       \\__").append('\n');
+        sb.append(" /             \\").append('\n');
+        sb.append("/_______________\\");
+        System.out.print(sb);
+    }
+}
